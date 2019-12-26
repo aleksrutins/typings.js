91ab5314270cd73c2a636bc26c22a537a0fa9593
munchkinhalfling <munchkinhalfling@pm.me>
2019-12-26 13:04:01 +0000

    Add demo

--- README.md
+++ README.md
@@ -2,23 +2,26 @@
 A typing system for JavaScript that ACTUALLY WORKS!
 
 ## Getting Started
-To use typings.js, just include `main.js` in your HTML.
-```html
-...
-<script src="/path/to/typings.js/main.js"></script>
-...
+To use typings.js, use ES modules to include the main script:
+```js
+import * as typings from 'typings.js';
+// Use the typings object here
 ```
-## The `typings` object
-The namespace for typings.js is called `typings`. It has the following interface:
-```WebIDL
-namespace typings {
-  //TypeName<T> is a string or a constructor.
-  //BasicConstructor<T> is a constructor.
-  //ClassProtoConstructor is a function that takes in an object of generic types with string keys and TypeName<any> values.
-  callback Observable<T> = T?(optional T? val);
-  Observable<T> createTypedVar<T extends Object>(TypeName<T> tipe, T value);
-  any createGenericClass(sequence<string> genArgs, ClassProtoConstructor proto);
-  boolean typeCheck<T>(TypeName<T> tipe, T obj);
-  T instantiate<T>(TypeName<T> tipe);
-}
+If you want to use it in your TypeScript code and include the `TypeName<T>` alias as well:
+```ts
+import * as typings from 'typings.js/main';
 ```
+## Type definition
+To save you some poking around, here are the types for everything in the library:
+```ts
+export type TypeName<T> = string | BasicConstructor<T>;
+type BasicConstructor<T> = () => void;
+type ClassProtoConstructor = (tipes: Object) => Object;
+export function createTypedVar<T extends object>(tipe: TypeName<T>, initialValue: T);
+export function createGenericClass(genericArgumentNames: string[], proto: ClassProtoConstructor);
+export function typeCheck<T>(tipe: TypeName<T>, obj: T): boolean
+export function instantiate<T>(tipe: TypeName<T>): T
+```
+## Tutorial
+Now that you've gotten Typings.js set up, let's go through a tutorial. This assumes that you are using JavaScript.
+You can see the full source code at [demo.html](https://github.com/munchkinhalfling/blob/demo/demo.html).
--- main.ts
+++ main.ts
@@ -1,68 +1,66 @@
-namespace typings {
-  export type TypeName<T> = string | Function;
-  type BasicConstructor<T> = () => void;
-  type ClassProtoConstructor = (tipes: Object) => Object;
-  // Type mappings. Key is type name, value is a function that:
-  // Returns true if x is of the key type
-  // Returns false otherwise.
-  // And a function that creates the type.
-  const types: Map<string, Function[]> = new Map<string, Function[]>([
-    ["int", [x => Number.isInteger(x), () => 0]],
-    ["string", [x => typeof x === "string", () => ""]],
-    ["float", [x => !isNaN(parseFloat(x)), () => 0.0]],
-    ["bool", [x => typeof x === "boolean", () => false]]
-  ]);
-  //create a typed variable. type is the type name or constructor, and value is the value.
-  export function createTypedVar<T extends object>(
-    tipe: TypeName<T>,
-    value: T
-  ) {
-    let _val: T = value;
-    let res = function() {
-      if (arguments.length >= 1) {
-        if (!typeCheck(tipe, arguments[0])) {
-          throw new TypeError(
-            "Expected type: " + tipe + ", got: " + typeof arguments[0]
-          );
-        } else {
-          _val = arguments[0];
-        }
-        return;
+export type TypeName<T> = string | Function;
+type BasicConstructor<T> = () => void;
+type ClassProtoConstructor = (tipes: Object) => Object;
+// Type mappings. Key is type name, value is a function that:
+// Returns true if x is of the key type
+// Returns false otherwise.
+// And a function that creates the type.
+const types: Map<string, Function[]> = new Map<string, Function[]>([
+  ["int", [x => Number.isInteger(x), () => 0]],
+  ["string", [x => typeof x === "string", () => ""]],
+  ["float", [x => !isNaN(parseFloat(x)), () => 0.0]],
+  ["bool", [x => typeof x === "boolean", () => false]]
+]);
+//create a typed variable. type is the type name or constructor, and value is the value.
+export function createTypedVar<T extends object>(
+  tipe: TypeName<T>,
+  value: T
+) {
+  let _val: T = value;
+  let res = function() {
+    if (arguments.length >= 1) {
+      if (!typeCheck(tipe, arguments[0])) {
+        throw new TypeError(
+          "Expected type: " + tipe + ", got: " + typeof arguments[0]
+        );
+      } else {
+        _val = arguments[0];
       }
-      return _val;
-    };
-    return res;
-  }
-  export function createGenericClass(
-    genArgs: string[],
-    proto: ClassProtoConstructor
-  ) {
-    let res = (...args: TypeName<any>[]) => {
-      let i = 0;
-      var typeArgs = {};
-      for (let arg of genArgs) {
-        typeArgs[arg] = args[i++];
-      }
-      let res = function() {
-        let __this__ = proto(typeArgs);
-        proto(typeArgs).constructor(__this__, ...arguments);
-        return __this__;
-      };
-      res.prototype = proto(typeArgs);
-      return res;
-    };
-    return res;
-  }
-  export function typeCheck<T>(tipe: TypeName<T>, obj: T): boolean {
-    if (typeof tipe === "string") {
-      return types.get(<string>tipe)[0](obj);
-    } else {
-      return obj instanceof <Function>tipe;
+      return;
     }
+    return _val;
+  };
+  return res;
+}
+export function createGenericClass(
+  genArgs: string[],
+  proto: ClassProtoConstructor
+) {
+  let res = (...args: TypeName<any>[]) => {
+    let i = 0;
+    var typeArgs = {};
+    for (let arg of genArgs) {
+      typeArgs[arg] = args[i++];
+    }
+    let res = function() {
+      let __this__ = proto(typeArgs);
+      proto(typeArgs).constructor(__this__, ...arguments);
+      return __this__;
+    };
+    res.prototype = proto(typeArgs);
+    return res;
+  };
+  return res;
+}
+export function typeCheck<T>(tipe: TypeName<T>, obj: T): boolean {
+  if (typeof tipe === "string") {
+    return types.get(<string>tipe)[0](obj);
+  } else {
+    return obj instanceof <Function>tipe;
   }
-  export function instantiate<T>(tipe: TypeName<T>): T {
-    return typeof tipe === "string"
-      ? types.get(<string>tipe)[1]()
-      : new (<BasicConstructor<T>>tipe)();
-  }
+}
+export function instantiate<T>(tipe: TypeName<T>): T {
+  return typeof tipe === "string"
+    ? types.get(<string>tipe)[1]()
+    : new (<BasicConstructor<T>>tipe)();
 }
